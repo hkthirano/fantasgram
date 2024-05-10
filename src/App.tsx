7cd25017a33ed13394ee7904c78aa58fe04b28f0
hokuto.hirano <hirano.hkt@gmail.com>
2024-05-10 17:34:13 +0900

    fix layout

--- src/App.tsx
+++ src/App.tsx
@@ -5,48 +5,46 @@ import { Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/m
 
 function App() {
   return (
-    <div className='p-16'>
-      <div className="h-screen w-screen flex justify-center">
-        <div className="flex-col space-y-4">
+    <div className="py-8 h-screen w-screen flex justify-center">
+      <div className="flex-col space-y-4">
 
-          <Card sx={{ maxWidth: 345 }}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="140"
-                image="https://fantasgramdata.z11.web.core.windows.net/salt_macro.jpg"
-                alt="塩"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  塩
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  2024/5/9
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image="https://fantasgramdata.z11.web.core.windows.net/salt_macro.jpg"
+              alt="塩"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                塩
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                2024/5/9
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
 
-          <Card sx={{ maxWidth: 345 }}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="140"
-                image="https://fantasgramdata.z11.web.core.windows.net/salt_micro.jpg"
-                alt="塩"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  塩
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  2024/5/9
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        </div>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height="140"
+              image="https://fantasgramdata.z11.web.core.windows.net/salt_micro.jpg"
+              alt="塩"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                塩
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                2024/5/9
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
       </div>
     </div>
   );
