e75e38134fed0cd8506ea7711ec5bcae7ffa6f98
hokuto.hirano <hirano.hkt@gmail.com>
2024-05-10 16:38:26 +0900

    installed mui and tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,53 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Card, CardActionArea, CardContent, CardMedia, Typography } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='p-16'>
+      <div className="h-screen w-screen flex justify-center">
+        <div className="flex-col space-y-4">
+
+          <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="140"
+                image="https://fantasgramdata.z11.web.core.windows.net/salt_macro.jpg"
+                alt="塩"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  塩
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  2024/5/9
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+
+          <Card sx={{ maxWidth: 345 }}>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="140"
+                image="https://fantasgramdata.z11.web.core.windows.net/salt_micro.jpg"
+                alt="塩"
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="div">
+                  塩
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  2024/5/9
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </div>
+      </div>
     </div>
   );
 }
